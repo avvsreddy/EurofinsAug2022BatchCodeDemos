44e15dad4f7c79d9ec610660e343dcbf823086d6
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-09-12 13:20:19 +0530

    Add project files.

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+namespace MyFirstConsoleApp
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int fno, sno, max;
+            Console.Write("Enter first number: ");
+            fno = int.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            sno = int.Parse(Console.ReadLine());
+
+            if (fno > sno)
+                max = fno;
+            else
+                max = sno;
+
+            Console.WriteLine($"The max of {fno} and {sno} is {max}");
+
+        }
+    }
+}
