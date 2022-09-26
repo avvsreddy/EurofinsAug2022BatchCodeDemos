c7b0a9627a90ec400bef53d5399a3cb261b8bfce
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-09-26 12:33:16 +0530

    del

---
+++ DelegatesDemo1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace DelegatesDemo1
+{
+
+    //class MyDelegate : MulticastDelegate
+    //{
+
+    //}
+
+    // Step 1: delegates declaration
+    delegate void MyDelegate(string str);
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // direct
+            //Program.Greeting("Hello");
+            // indirect
+            // Step 2: Instantiate and initialize
+            Program p = new Program();
+            MyDelegate delObj = new MyDelegate(p.Hello);
+
+            delObj += Greeting; // subscribing
+            delObj -= p.Hello; // unsubscribing
+
+            // Step 3: Delegate Invoking/Calling
+            //delObj.Invoke("Hello");
+            delObj("Ramesh");
+        }
+
+        public static void Greeting(string msg)
+        {
+            Console.WriteLine($"Greeting: {msg}");
+        }
+
+        public void Hello(string name)
+        {
+            Console.WriteLine($"Hello: {name}");
+        }
+    }
+}
---
+++ DelegatesDemo2/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace DelegatesDemo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc1 = new Account();
+            acc1.alert += Notification.SendEmail;
+            //acc1.Subscribe(Notification.SendEmail);
+            acc1.alert += Notification.SendSMS;
+            acc1.alert += Notification.SendWhatsApp;
+            acc1.alert -= Notification.SendEmail;
+            //acc1.Subscribe(Notification.SendSMS);
+            //acc1.Unsubscribe(Notification.SendEmail);
+            //acc1.alert("Credited $99999999999999999999999999.00");
+            System.Console.WriteLine($"Initial Balance : {acc1.Balance}");
+            //acc1.Deposit(5000);
+            System.Console.WriteLine($"After Deposit Balance : {acc1.Balance}");
+            //acc1.Withdraw(1000);
+            System.Console.WriteLine($"After Withdraw Balance : {acc1.Balance}");
+
+
+        }
+    }
+
+
+    // declare the delegate
+    public delegate void Alert(string msg);
+
+    class Account
+    {
+        public int Balance { get; private set; }
+        public event Alert alert = null; //new Alert(Notification.SendEmail);
+        //public void Subscribe(Alert alert)
+        //{
+        //    this.alert += alert;
+        //}
+        //public void Unsubscribe(Alert alert)
+        //{
+        //    this.alert -= alert;
+        //}
+
+        public void Deposit(int amount)
+        {
+            Balance += amount;
+            if (alert != null)
+                alert($"Credited {amount}");
+        }
+        public void Withdraw(int amount)
+        {
+            Balance -= amount;
+            if (alert != null)
+                alert($"Debited {amount}");
+        }
+    }
+
+
+    public class Notification
+    {
+        public static void SendEmail(string msg)
+        {
+            //SmtpClient
+            //MailMessage
+            System.Console.WriteLine($"MAIL: {msg}");
+        }
+
+        public static void SendSMS(string msg)
+        {
+            //SmtpClient
+            //MailMessage
+            System.Console.WriteLine($"SMS: {msg}");
+        }
+        public static void SendWhatsApp(string msg)
+        {
+            //SmtpClient
+            //MailMessage
+            System.Console.WriteLine($"Whats App: {msg}");
+        }
+
+    }
+}
