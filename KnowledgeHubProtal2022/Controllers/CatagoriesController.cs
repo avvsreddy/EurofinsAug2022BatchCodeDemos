12124f90e2b110ed8e6f311a9aa98e123e587ec0
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-10-10 10:59:25 +0530

    ef

--- KnowledgeHubProtal2022/Controllers/CatagoriesController.cs
+++ KnowledgeHubProtal2022/Controllers/CatagoriesController.cs
@@ -1,6 +1,5 @@
 ﻿using KnowledgeHubProtal2022.Models.Data;
 using KnowledgeHubProtal2022.Models.Entities;
-using System.Linq;
 using System.Web.Mvc;
 
 namespace KnowledgeHubProtal2022.Controllers
@@ -8,14 +7,15 @@ namespace KnowledgeHubProtal2022.Controllers
     public class CatagoriesController : Controller
     {
 
-        private KnowledgeHubDbContext db = new KnowledgeHubDbContext();
+        //private KnowledgeHubDbContext db = new KnowledgeHubDbContext();
+        private ICatagoriesRepository repo = new CatagoriesRepository();
 
         // GET: Catagories
         // .../catagories/index
         public ActionResult Index()
         {
             // fetch the catagories information from model/dal
-            var catagories = db.Catagories.ToList();
+            var catagories = repo.GetCatagories();
             // pass the data into view
 
             //ViewBag.Catagoreis = catagories;
@@ -35,17 +35,19 @@ namespace KnowledgeHubProtal2022.Controllers
             // validate
             if (!ModelState.IsValid)
                 return View("Create");
-            db.Catagories.Add(catagory);
-            db.SaveChanges();
+            //db.Catagories.Add(catagory);
+            //db.SaveChanges();
+            repo.Create(catagory);
             TempData["Message"] = $"Catagory {catagory.Name} Successfully Created";
             return RedirectToAction("Index");
         }
 
         public ActionResult Delete(int id)
         {
-            var catagoryToDelete = db.Catagories.Find(id);
-            db.Catagories.Remove(catagoryToDelete);
-            db.SaveChanges();
+            var catagoryToDelete = repo.GetCatagory(id);
+            //db.Catagories.Remove(catagoryToDelete);
+            //db.SaveChanges();
+            repo.Delete(id);
             TempData["Message"] = $"Catagory {catagoryToDelete.Name} Successfully Deleted";
             return RedirectToAction("Index");
         }
@@ -53,7 +55,7 @@ namespace KnowledgeHubProtal2022.Controllers
         [HttpGet]
         public ActionResult Edit(int id)
         {
-            var catagoryToEdit = db.Catagories.Find(id);
+            var catagoryToEdit = repo.GetCatagory(id);
             return View(catagoryToEdit);
         }
 
@@ -62,8 +64,9 @@ namespace KnowledgeHubProtal2022.Controllers
         {
             if (!ModelState.IsValid)
                 return View();
-            db.Entry(catagory).State = System.Data.Entity.EntityState.Modified;
-            db.SaveChanges();
+            //db.Entry(catagory).State = System.Data.Entity.EntityState.Modified;
+            //db.SaveChanges();
+            repo.Update(catagory);
             TempData["Message"] = $"Catagory {catagory.Name} Successfully Modified";
             return RedirectToAction("Index");
 
