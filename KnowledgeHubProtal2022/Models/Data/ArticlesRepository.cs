7e4e629f7b6e31b9e5a761707954ebba566e1a15
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-10-10 11:07:50 +0530

    ef

---
+++ KnowledgeHubProtal2022/Models/Data/ArticlesRepository.cs
@@ -0,0 +1,44 @@
+﻿using KnowledgeHubProtal2022.Models.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace KnowledgeHubProtal2022.Models.Data
+{
+    public class ArticlesRepository : IArticlesRepository
+    {
+        public void Approve(List<int> articleIds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Article GetArticle(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Article> GetArticlesForBrowse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Article> GetArticlesForReview()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reject(List<int> articleIds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Article> Search(string data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Submit(Article article)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ KnowledgeHubProtal2022/Models/Data/IArticlesRepository.cs
@@ -0,0 +1,19 @@
+﻿using KnowledgeHubProtal2022.Models.Entities;
+using System.Collections.Generic;
+
+namespace KnowledgeHubProtal2022.Models.Data
+{
+    public interface IArticlesRepository
+    {
+        void Submit(Article article);
+        List<Article> Search(string data);
+        Article GetArticle(int id);
+        void Approve(List<int> articleIds);
+        void Reject(List<int> articleIds);
+        List<Article> GetArticlesForReview();
+        List<Article> GetArticlesForBrowse();
+
+
+
+    }
+}
